fc4aa72974f1c7de29e5cc1b01f14a75da359cb9
flopmart <34133163+asadnisar98@users.noreply.github.com>
2023-05-22 17:37:30 +0500

    adding comments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,19 +2,23 @@ jQuery(document).ready(function($) {
   var $masthead = $('#horizontal-slider');
   var mastheadWidth = 0;
 
+  // Function to get the width of the masthead
   function getMastheadWidth() {
     mastheadWidth = $masthead.get(0).scrollWidth;
   }
   getMastheadWidth();
+
+  // Event listener to refresh the masthead width when ScrollTrigger refreshes
   ScrollTrigger.addEventListener('refreshInit', getMastheadWidth);
 
   function updateProxy() {
-    // move the handler to the corresponding ratio according to the page's scroll position.
+    // Move the handler to the corresponding ratio according to the page's scroll position.
     if (mastheadScrollTrigger) {
       gsap.set(proxy, { x: -mastheadScrollTrigger.scroll(), overwrite: 'auto' });
     }
   }
 
+  // Create a ScrollTrigger to animate the horizontal slider
   var mastheadScrollTrigger = ScrollTrigger.create({
     id: 'horizontal-slider',
     animation: gsap.to('#horizontal-slider', {
@@ -33,6 +37,7 @@ jQuery(document).ready(function($) {
     invalidateOnRefresh: true,
   });
 
+  // Create a proxy element for Dragging
   var proxy = document.createElement('div');
   var draggable = Draggable.create(proxy, {
     trigger: '#horizontal-slider',
@@ -47,20 +52,17 @@ jQuery(document).ready(function($) {
     },
   })[0];
 
+  // Event listener to update the proxy element when scrolling with the mouse wheel
   window.addEventListener('wheel', updateProxy);
 
   // Next and Back Buttons functionality
   $('.slider-next-arrow').click(function(e) {
-   
     e.preventDefault();
     mastheadScrollTrigger.scroll(mastheadScrollTrigger.scroll() + 400); // Adjust the scroll amount as per your requirement
   });
 
   $('.slider-prev-arrow').click(function(e) {
-
     e.preventDefault();
     mastheadScrollTrigger.scroll(mastheadScrollTrigger.scroll() - 400); // Adjust the scroll amount as per your requirement
-
-
   });
 });
